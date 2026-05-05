f778427c9b77452020328eb862c7f5026433cd9b
Nunes <Nunes@DESKTOP-TTB5CJ4>
2026-05-05 19:30:54 -0300

    ajustes.

--- Pages/Menu/Home/HomeContext.axaml.cs
+++ Pages/Menu/Home/HomeContext.axaml.cs
@@ -1,4 +1,8 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using ScottPlot;
+using ScottPlot.Avalonia;
+using System.Collections.Generic;
 
 namespace FinanceProject;
 
@@ -8,5 +12,52 @@ public partial class HomeContext : UserControl
     {
         InitializeComponent();
         this.DataContext = this;
+    }
+
+    protected override void OnLoaded(RoutedEventArgs e)
+    {
+        base.OnLoaded(e);
+        if (Design.IsDesignMode)
+            return;
+
+        ConfigurarGraficoLinha();
+        //ConfigurarGraficoPizza();
+    }
+
+    private void ConfigurarGraficoLinha()
+    {
+        var plot = this.FindControl<AvaPlot>("PlotLinha");
+        if (plot == null) return;
+
+        double[] dataY = { 10, 15, 12, 18, 20 };
+        plot.Plot.Add.Signal(dataY);
+
+        // Cores e Estilo
+        plot.Plot.FigureBackground.Color = Color.FromHex("#1F222C");
+        plot.Plot.DataBackground.Color = Color.FromHex("#1F222C");
+        plot.Plot.Axes.Color(Color.FromHex("#FFFFFF"));
+
+        plot.Refresh();
+    }
+
+    private void ConfigurarGraficoPizza()
+    {
+        var plot = this.FindControl<AvaPlot>("PlotPizza");
+        if (plot == null) return;
+
+        List<PieSlice> slices = new()
+        {
+            new() { Value = 40, Fill = new FillStyle { Color = Color.FromHex("#7FD9F9") }, Label = "Salário" },
+            new() { Value = 60, Fill = new FillStyle { Color = Color.FromHex("#F87171") }, Label = "Despesas" },
+        };
+
+        var pie = plot.Plot.Add.Pie(slices);
+        pie.DonutFraction = 0.5; // Deixa com cara de gráfico de rosca (opcional)
+
+        plot.Plot.FigureBackground.Color = Color.FromHex("#1F222C");
+        plot.Plot.Axes.Frameless();
+        plot.Plot.HideGrid();
+
+        plot.Refresh();
     }
 }
